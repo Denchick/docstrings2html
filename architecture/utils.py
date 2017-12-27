5237680491235694df2ace4fb7b717c775e80d2b
Volkov Denis <denchick1997@mail.com>
2017-12-27 17:07:41 +0500

    refactor utils.py

--- architecture/utils.py
+++ architecture/utils.py
@@ -11,21 +11,25 @@ def ignore_function(ignore):
         return set(ignored_names)
     return _ignore_
 
+
 def include_only_function(*includes):
-    """ Ignore all except *include files extensions, non-hidded folders and __pycache__.
+    """ Ignore all except *include files extensions,
+    non-hidded folders and __pycache__.
      Use for shutil.copytree """
     def _ignore_(path, names):
         ignored_names = []
         for name in names:
             for include in includes:
-                if not name.endswith(include) and '.' in name or name == '__pycache__':
+                if not name.endswith(include) and '.' in name or '__' in name:
                     ignored_names.append(name)
         return set(ignored_names)
     return _ignore_
 
+
 def get_text_from_file(filename):
     with open(filename, 'r', encoding='utf-8') if filename else sys.stdin as f:
         return f.read()
+
 
 def walk_through_files(path, file_extension='.py'):
     for (dirpath, dirnames, filenames) in os.walk(path):
@@ -33,11 +37,13 @@ def walk_through_files(path, file_extension='.py'):
             if filename.endswith(file_extension):
                 yield os.path.join(dirpath, filename)
 
+
 def walk_through_directories(path):
     yield path
     for (dirpath, dirnames, filenames) in os.walk(path):
         for dirname in dirnames:
             yield os.path.join(dirpath, dirname)
+
 
 def write_to_file(filename, text):
     with open(filename, 'w') if filename else sys.stdout as f:
